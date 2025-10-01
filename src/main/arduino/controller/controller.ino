3ad69412f42d544ae4ee752e11f27209f8f58910
josh-r-dick <josh.r.dick@greenimaging.com>
2025-10-01 11:38:53 -0300

    Change drone connection based on right switch
    Putting the switch to the left connects to drone 100000, to the right it connects to drone 200000.
    
    Also updated the display to print the controller values.

--- src/main/arduino/controller/controller.ino
+++ src/main/arduino/controller/controller.ino
@@ -17,6 +17,16 @@ int RIGHT_SWITCH_1 = 3; // Right switch to the left
 int RIGHT_SWITCH_2 = 2; // Right switch to the right
 int DIAL_PIN = A0;
 
+const byte address[2][6] = {"100000", "200000"}; // Address needs to match reciever addresses.
+const byte currentAddress[6];
+
+int currentAddressIndex = -1; // The index of the address being used, initialized to -1 so it will be connected on first run
+
+// Variables to handle LCD update timing
+// LCD is 16x2
+unsigned long lcdUpdateTime = 0;
+const unsigned long lcdUpdateInterval = 100; // Update interval in milliseconds
+
 // Struct representing the control data coming from the remote. Should not exceed 32 bytes.
 struct ControlData {
   byte throttle;
@@ -33,50 +43,82 @@ struct ControlData {
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-
-// const uint64_t pipeOut = 0xE8E8F0F0E1LL; //IMPORTANT: The same as in the receiver!!!
-// todo Change this based on right switch position.
-const byte address[6] = "200000";// Address needs to match reciever.
-
 RF24 radio(10, 9); // select  CE and CSN  pins
 
 ControlData data;
 
-// Variables to handle LCD update timing
-unsigned long lcdUpdateTime = 0;
-const unsigned long lcdUpdateInterval = 100; // Update interval in milliseconds
-
-void resetData()
-{
-  //This are the start values of each channal
-  // Throttle is 0 in order to stop the motors
-  //127 is the middle value of the 10ADC.
-
-  data.throttle = 0;
-  data.yaw = 127;
-  data.pitch = 127;
-  data.roll = 127;
-  data.dial = 0;
-  data.leftSwitchPosition1 = 0;
-  data.leftSwitchPosition2 = 0;
-  data.rightSwitchPosition1 = 0;
-  data.rightSwitchPosition2 = 0;
-}
-
+/**
+ * Setup the hardware.
+ */
 void setup()
 {
-  //Start everything up
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  // starting the Serial Monitor
+  Serial.begin(9600);               
 
-  // radio.setAutoAck(false);
-  // radio.setDataRate(RF24_250KBPS);
-  // radio.openWritingPipe(address);
+  initLCD();
+
+  // Initialize the digital pins
+  pinMode(LEFT_SWITCH_1, INPUT_PULLUP);
+  pinMode(LEFT_SWITCH_2, INPUT_PULLUP);
+  pinMode(RIGHT_SWITCH_1, INPUT_PULLUP);
+  pinMode(RIGHT_SWITCH_2, INPUT_PULLUP);
+
   resetData();
+  readData();
+}
 
-  lcd.init();                          // initialize the lcd
+/** 
+ * Main loop 
+ */
+void loop()
+{
+  // Connect the radio to the proper channel.
+  connectRadio();
+
+  // // Update LCD display at regular intervals
+  // unsigned long currentMillis = millis();
+  // if (currentMillis - lcdUpdateTime >= lcdUpdateInterval) {
+  //   updateLCD();
+  //   lcdUpdateTime = currentMillis;
+  // }
+
+  // Read the data.
+  readData();
+  // Write the data
+  radio.write(&data, sizeof(ControlData));
+
+  delay(250);
+}
+
+/** Connect the radio to the proper channel if the switch changed */
+void connectRadio()
+{
+    int newAddressIndex = 0;
+       // Right switch is to the left
+    if(data.rightSwitchPosition1 == 1 && data.rightSwitchPosition2 == 0) {
+      newAddressIndex = 0;
+    }
+
+    // Right switch is to the right
+    if(data.rightSwitchPosition1 == 0 && data.rightSwitchPosition2 == 1) {
+      newAddressIndex = 1;
+    }
+
+    // If the address is changed by the switch, connect to the proper channel.
+    if(currentAddressIndex != newAddressIndex) {
+      Serial.print("Changing to address ");
+      Serial.println(newAddressIndex);
+      currentAddressIndex = newAddressIndex;
+      memcpy(currentAddress, address[currentAddressIndex], 6 * sizeof(byte));
+      initRadioComms();
+    }
+
+}
+
+/** Initialize the LCD screen.*/
+void initLCD()
+{
+  lcd.init();                         
   lcd.backlight();
   lcd.setCursor(1, 0);
   lcd.clear();
@@ -91,19 +133,12 @@ void setup()
   delay(2200);
   lcd.clear();
   pinMode(vdividerPin, INPUT);
-  Serial.begin(9600);               // starting the Serial Monitor
-
-  pinMode(LEFT_SWITCH_1, INPUT_PULLUP);
-  pinMode(LEFT_SWITCH_2, INPUT_PULLUP);
-  pinMode(RIGHT_SWITCH_1, INPUT_PULLUP);
-  pinMode(RIGHT_SWITCH_2, INPUT_PULLUP);
-
 }
 
-/**************************************************/
-
-// Returns a corrected value for a joystick position that takes into account
-// the values of the outer extents and the middle of the joystick range.
+/** 
+ * Returns a corrected value for a joystick position that takes into account
+ * the values of the outer extents and the middle of the joystick range.
+ */
 int mapJoystickValues(int val, int lower, int middle, int upper, bool reverse)
 {
   val = constrain(val, lower, upper);
@@ -114,17 +149,9 @@ int mapJoystickValues(int val, int lower, int middle, int upper, bool reverse)
   return ( reverse ? 255 - val : val );
 }
 
-void loop()
+/** Read the data from the switches, joysticks and knobs. */
+void readData()
 {
-  unsigned long currentMillis = millis();
-
-  // Update LCD display at regular intervals
-  if (currentMillis - lcdUpdateTime >= lcdUpdateInterval) {
-    updateLCD();
-    lcdUpdateTime = currentMillis;
-  }
-
-
   // The calibration numbers used here should be measured
   // for your joysticks till they send the correct values.
   data.throttle = mapJoystickValues( analogRead(throttlePin), 13, 524, 1015, true );
@@ -136,26 +163,38 @@ void loop()
   data.leftSwitchPosition2 = digitalRead(LEFT_SWITCH_2);
   data.rightSwitchPosition1 = digitalRead(RIGHT_SWITCH_1);
   data.rightSwitchPosition2 = digitalRead(RIGHT_SWITCH_2);
-
+  
+  // Print it.
   printData(data);
-
-  // todo If the right switch changes, set the radio address re-initialize it.
-
-  radio.write(&data, sizeof(ControlData));
-  delay(250);
 }
 
-/**
- * Initialize the radio communications.
- */
+/**  Initialize the radio communications. */
 void initRadioComms() {
+  Serial.println("Initializing radio communications.");
   radio.begin();
-  radio.openReadingPipe(0, address);
+  radio.openWritingPipe(currentAddress);
   radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+  radio.stopListening();
 }
 
+/** Reset the data. */
+void resetData()
+{
+  //This are the start values of each channal
+  // Throttle is 0 in order to stop the motors
+  //127 is the middle value of the 10ADC.
+  data.throttle = 0;
+  data.yaw = 127;
+  data.pitch = 127;
+  data.roll = 127;
+  data.dial = 0;
+  data.leftSwitchPosition1 = 0;
+  data.leftSwitchPosition2 = 0;
+  data.rightSwitchPosition1 = 0;
+  data.rightSwitchPosition2 = 0;
+}
 
+/** Print the data. */
 void printData(ControlData toPrint) {
     Serial.print("Throttle: ");
     Serial.print(toPrint.throttle);
@@ -176,8 +215,29 @@ void printData(ControlData toPrint) {
     Serial.print(" Right Switch 2: ");
     Serial.print(toPrint.rightSwitchPosition2);
     Serial.println("");
+
+    // Build strings to print to the lcd.
+    String line1, line2;
+    line1 += toPrint.throttle ;
+    line1 += " ";
+    line1 += toPrint.yaw;
+    line1 += " ";
+    line1 += toPrint.pitch;
+    line1 += " ";
+    line1 += toPrint.roll;
+
+    line2 += toPrint.dial ;
+    line2 += " ";
+    line2 += toPrint.leftSwitchPosition1;
+    line2 += toPrint.leftSwitchPosition2;
+    line2 += " ";
+    line2 += toPrint.rightSwitchPosition1;
+    line2 += toPrint.rightSwitchPosition2;
+
+    printToLcd(line1, line2);
 }
 
+/** Write to the lcd. */
 void updateLCD() {
   input = analogRead(vdividerPin);
   vol = (input * 10.0) / 1024.0;
@@ -189,3 +249,15 @@ void updateLCD() {
   lcd.print(vol);
   lcd.print("Vx");
 }
+
+
+
+void printToLcd(String firstLine, String secondLine) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(firstLine);
+  lcd.setCursor(0, 1);
+  lcd.print(secondLine);
+}
+
+
